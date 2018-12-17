e1dd9d2c609e2b91ef94bf7d78101efc62186c48
root <root@localhost.localdomain>
2018-12-17 17:13:05 +0000

    changed port param

--- app.js
+++ app.js
@@ -101,10 +101,7 @@ app.get('*', function(req, res) {
 
 // SERVER
 
+app.listen(process.env.PORT || 5000);
 
 
 
-
-app.listen(process.env.PORT, process.env.IP, function(){
-	console.log("Server started");
-});
