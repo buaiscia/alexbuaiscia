045f855d5157d5f53ef302240f290bb405616fbe
root <root@localhost.localdomain>
2018-12-17 11:50:42 +0000

    console msg on start server

--- app.js
+++ app.js
@@ -103,4 +103,6 @@ app.get('*', function(req, res) {
 //     console.log("server started on port 4000");
 // });
 
-app.listen(process.env.PORT, process.event.IP);
+app.listen(process.env.PORT, process.env.IP, function(){
+   console.log("The server has started!");
+});
