fa7b9babfc85fa11a4322f5fea52c0258db6f8e2
Alex Buaiscia (HCI) <alex.buaiscia@gmail.com>
2019-12-10 14:44:46 +0100

    add 3 routes + correspondent ejs for portfolio projects

--- routes/dev/portfolio.js
+++ routes/dev/portfolio.js
@@ -10,6 +10,25 @@ router.get("/", function(req, res, next) {
     res.render("dev/portfolio");
 });
 
+//PROJECTS
+
+router.get("/website", function(req, res, next) {
+
+    res.render("dev/portfolio/website");
+});
+
+
+router.get("/deploymentTool", function(req, res, next) {
+
+    res.render("dev/portfolio/deploymentTool");
+});
+
+
+router.get("/portal", function(req, res, next) {
+
+    res.render("dev/portfolio/portal");
+});
+
 
 
 
