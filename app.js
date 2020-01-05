dd9296db6f3acc68fa8b2fb678627ae9a36baf49
Alex Buaiscia (HCI) <alex.buaiscia@gmail.com>
2020-01-05 22:55:28 +0100

    typo

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.locals.moment = require('moment');
 
 var http = require("http");
 setInterval(function() {
-    http.get("https://alexbuaiscia.herokuapp.com/");
+    http.get("http://alexbuaiscia.herokuapp.com/");
 }, 300000); // every 5 minutes (300000)
 
 // SETTING OTHER STUFF
